692af76ce1f33e2dffb0ab818d1ef6fe22bbd4b2
Bruna Verrone <ti.agosem@hotmail.com>
2025-04-03 23:41:19 -0300

    Remove favicon and add Tailwind CSS configuration; update Next.js config and package dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "picsum.photos",
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
