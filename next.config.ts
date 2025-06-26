8208d28e20b948f93f471d827cb7af69e4b71755
Bruna Verrone <ti.agosem@hotmail.com>
2025-06-25 23:21:34 -0300

    Add image domains to next.config.ts for enhanced image support

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      "picsum.photos",
+      "www.coursera.org",
+      "cursos.alura.com.br",
+      "e-certificado.com"
     ],
     formats: ["image/avif", "image/webp"],
   },
